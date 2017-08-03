212ed7bf64f9674020f1a679ab96a51aa39bcf32
wkm23@126.com <wkm23@126.com>
2017-08-04 00:10:31 +1200

    child worker listen on uncaughtException and exit

--- server/index.js
+++ server/index.js
@@ -101,4 +101,10 @@ if (cluster.isMaster){
     });
   });
 
+  process.on('uncaughtException', function (err) {
+    console.error((new Date).toUTCString() + ' uncaughtException:', err.message);
+    console.error(err.stack);
+    process.exit(1);
+  })
+
 }
