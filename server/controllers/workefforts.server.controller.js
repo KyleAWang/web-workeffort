3bd5751887212b59f6893fd73b19704def4ae22c
wkm23@126.com <wkm23@126.com>
2017-10-14 23:48:52 +1300

    getWorkEffortEventsByPeriod extract params

--- server/controllers/workefforts.server.controller.js
+++ server/controllers/workefforts.server.controller.js
@@ -55,7 +55,43 @@ export function listWorkEffort(req, res) {
 }
 
 export function getWorkEffortEventsByPeriod(req, res) {
-
+  const params = [];
+  if (req.body.calendarType) {
+    params.body.calendarType = req.body.calendarType;
+  }
+  if (req.body.partyId) {
+    params.body.partyId = req.body.partyId;
+  }
+  if (req.body.partyIds) {
+    params.body.partyIds = req.body.partyIds;
+  }
+  if (req.body.facilityId) {
+    params.body.facilityId = req.body.facilityId;
+  }
+  if (req.body.workEffortTypeId) {
+    params.body.workEffortTypeId = req.body.workEffortTypeId;
+  }
+  if (req.body.start) {
+    params.body.start = req.body.start;
+  }
+  if (req.body.numPeriods) {
+    params.body.numPeriods = req.body.numPeriods;
+  }
+  if (req.body.periodType) {
+    params.body.periodType = req.body.periodType;
+  }
+  if (req.body.filterOutCanceledEvents) {
+    params.body.filterOutCanceledEvents = req.body.filterOutCanceledEvents;
+  }
+  if (req.body.entityExprList) {
+    params.body.entityExprList = req.body.entityExprList;
+  }
+  if (req.body.username) {
+    params.body.username = req.body.username;
+  }
+  if (req.body.password) {
+    params.body.password = req.body.password;
+  }
 }
 
 export function createWorkEffort(req, res) {
